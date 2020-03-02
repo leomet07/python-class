de2b6cd628b60d9a8184f8d5da46014c55f85955
Leonid <47071760+leomet07@users.noreply.github.com>
2020-03-02 12:02:04 -0500

    Board making and printing

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,1 +1,29 @@
+print("Tic tac toe")
+board = []
+
+dim = 3
+# create board to be dim * dim
+for y in range(0,dim):
+    row = []
+    for x in range(0,dim):
+        row.append("")
+
+    board.append(row)
+
+
+# neatly print a board
+def neat_board(board):
+    #prints the board
+    print("-----------")
+    for collumn in board:
+
+        print(collumn)
+# To access board use
+#board[y][x]  
+
+neat_board(board)
+
+
+
+
 
